acae3da12d5635a212fc6301355c337687af7ffc
freshhoe <yh91211@gmail.com>
2021-09-22 21:27:05 +0900

    feat: Add deepClone function

---
+++ hw1/deepClone.js
@@ -0,0 +1,26 @@
+function deepClone(object) {
+  function getType(object) {
+    return Object.prototype.toString.call(object).slice(8, -1).toLowerCase();
+  }
+
+  function cloneArray() {
+    return object.map(item => deepClone(item));
+  }
+
+  function cloneObject() {
+    const clone = {};
+
+    for (let key in object) {
+      if (object.hasOwnProperty(key)) clone[key] = deepClone(object[key]);
+    }
+
+    return clone;
+  }
+
+  const objectType = getType(object);
+  if (objectType === 'array') return cloneArray();
+  if (objectType === 'object') return cloneObject();
+  return object;
+}
+
+export default deepClone;
