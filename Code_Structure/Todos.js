6082e40d10848088f13e1639236145e4a637b23f
freshhoe <yh91211@gmail.com>
2021-10-05 21:40:33 +0900

    Learn and exercise about code structure

---
+++ Code_Structure/Todos.js
@@ -0,0 +1,26 @@
+// 1.3 모듈화
+let todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+const add = newTodo => {
+  todos = [newTodo, ...todos];
+};
+
+const render = () =>
+  todos
+    .map(
+      ({ id, content, completed }) => `
+            <li id="${id}">
+            <label><input type="checkbox" ${
+              completed ? 'checked' : ''
+            }>${content}</label>
+            </li>`
+    )
+    .join('');
+
+const Todos = { add, render };
+
+export default Todos;
---
+++ Code_Structure/app.js
@@ -0,0 +1,5 @@
+import Todos from './Todos.js';
+console.log(Todos);
+
+Todos.add({ id: 4, content: 'React', completed: false });
+console.log(Todos.render());
